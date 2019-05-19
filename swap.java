8622f09a36b126a2e81ac7ccdb7cf398bdf7877b
srvn1397 <50725180+srvn1397@users.noreply.github.com>
2019-05-19 10:10:53 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,12 @@
+class swap
+{
+public static void main(String arg[])
+{
+int s=10,a=100,t;
+System.out.println("before swapping..s="+s+",a="+a);
+t=s;
+s=a;
+a=t;
+System.out.println("after swapping..s="+s+",a="+a);
+}
+}
